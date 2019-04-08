f6015667dfbaf1751208646e16a0e6ec749111aa
hppod <hppodgornik@gmail.com>
2019-04-07 21:43:38 -0300

    onLongPress para flag no field

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Alert } from 'react-native';
 
 import params from './src/params'
 import MineField from './src/components/MineField'
-import { createMinedBoard, cloneBoard, openField, hadExplosion, wonGame, showMines } from './src/functions'
+import { createMinedBoard, cloneBoard, openField, hadExplosion, wonGame, showMines, invertFlag } from './src/functions'
 
 export default class App extends React.Component {
 
@@ -46,6 +46,18 @@ export default class App extends React.Component {
     this.setState({ board, lost, won })
   }
 
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board, row, column)
+    const won = wonGame(board)
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você venceu!')
+    }
+
+    this.setState({ board, won })
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -53,7 +65,8 @@ export default class App extends React.Component {
         <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
         <View style={styles.board}>
           <MineField board={this.state.board}
-            onOpenField={this.onOpenField} />
+            onOpenField={this.onOpenField}
+            onSelectField={this.onSelectField} />
         </View>
       </View>
     );
